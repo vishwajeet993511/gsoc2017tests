ccb3437e0c5d0fbebb4ec4ab5b686fc59992318a
vishwajeet shukla <vishwajeet993511@gmail.com>
2017-03-19 16:30:55 +0530

    added comments

--- mediumpart1.R
+++ mediumpart1.R
@@ -4,8 +4,10 @@ require(rvest)
 #Sys.setenv(http_proxy="")
 
 url <- "http://ftp.funet.fi/pub/sci/bio/life/insecta/lepidoptera/ditrysia/papilionoidea/papilionidae/papilioninae/lamproptera/"
+# reading url
 readhtml <- read_html(url)
+# using rvest library to get the node text
 species <- readhtml %>% html_nodes(".NAMES .SN li") %>% html_text()
 speciesListfinal <- gsub("=|;.*","",species)
-
+# converting and storing  to csv files
 write.csv(speciesListfinal,"mediumpart1speciesList.csv")
