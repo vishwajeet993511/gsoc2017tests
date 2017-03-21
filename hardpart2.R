ce0fcb28c4096ea0d4db3e665ab4fa04a976550b
vishwajeet shukla <vishwajeet993511@gmail.com>
2017-03-21 19:33:01 +0530

    modified hardpart1.R

--- hardpart2.R
+++ hardpart2.R
@@ -1,15 +1,30 @@
 tocsv <- function(textfile){
+  library(stringr)
   data <- read.table(textfile,sep = "\t")
-  distribution <- grep("^Distribution", data$V1)
+  #distribution <- grep("^Distribution", data$V1)
   namesindex <- grep("^[0-9]",data$V1)
   
-  DISTRIBUTION <- data[distribution,]
+  #DISTRIBUTION <- data[distribution,]
   NAMES <- data[namesindex,]
   NAMES <- sub("^[0-9].|^[0-9][0-9]. ","",NAMES)
-  col1 <- data$V1[1]
-  finaldata <- data.frame(col1,NAMES,DISTRIBUTION)
+  #col1 <- data$V1[1]
+  #finaldata <- data.frame(col1,NAMES,DISTRIBUTION)
   
-  finaldata
+  stringnew <- paste(data$V1,collapse = " ")
+  
+  
+  #x <- str_locate_all(pattern ='[0-9]. ',stringnew)
+  
+  y <- str_locate_all(pattern ='Distribution.*?[0-9.]',stringnew)
+  y <- data.frame(y)
+  distributionlist <- character(length(y$start))
+  for(i in 1:length(y$start))
+    distributionlist[i] <- substr(stringnew,y$start[i],y$end[i]-2)
+  
+  distributionlist <- gsub("^Distribution: ","",distributionlist)
+  finaldata2 <- data.frame("NAME"= NAMES , "DISTRIBUTION"= distributionlist)
+  #write.csv(finaldata2,"abc.csv")
+  finaldata2
   
 }
 
