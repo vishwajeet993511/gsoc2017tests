0b52f8b6374e31038bef0dfa2f55ca2b37e898b2
vishwajeet shukla <vishwajeet993511@gmail.com>
2017-03-21 16:08:07 +0530

    modified medium part2 function

--- mediumpart2.R
+++ mediumpart2.R
@@ -1,16 +1,35 @@
 speciesnames <- function(url){
-require(RCurl)
-require(XML)
-require(rvest) 
-#Sys.setenv(http_proxy="")
-
-#url <- "http://ftp.funet.fi/pub/sci/bio/life/insecta/lepidoptera/ditrysia/papilionoidea/papilionidae/papilioninae/lamproptera/"
-readhtml <- read_html(url)
-species <- readhtml %>% html_nodes(".NAMES .SN li") %>% html_text()
-speciesListfinal <- gsub("=|;.*","",species)
-
-speciesListfinal
-
+  require(RCurl)
+  require(XML)
+  require(rvest) 
+  #Sys.setenv(http_proxy="http://10.3.100.207:8080")
+  
+  #url <- "http://ftp.funet.fi/pub/sci/bio/life/insecta/lepidoptera/ditrysia/papilionoidea/papilionidae/papilioninae/lamproptera/"
+  # reading url
+  readhtml <- read_html(url)
+  # using rvest library to get the node text
+  species <- readhtml %>% html_nodes(".NAMES .SN li") %>% html_text()
+  speciesListfinal <- gsub("=|;.*","",species)
+  speciesListfinal <-  gsub("^ ","",speciesListfinal)
+  
+  
+  splitstring <- character(length(speciesListfinal))
+  genus <- character(length(speciesListfinal))
+  for(i in 1:length(speciesListfinal))
+  {
+    if((length(unlist(strsplit(speciesListfinal[i],split = " "))) > 3) | (grepl(",",speciesListfinal[i])== FALSE ))
+      splitstring[i] <- unlist(strsplit(speciesListfinal[i],split = " "))[2]
+    else
+      splitstring[i] <- "-"
+    
+  }
+  for(i in 1:length(speciesListfinal))
+  {
+    genus[i] <- unlist(strsplit(speciesListfinal[i],split = " "))[1]
+  }
+  
+  dataframe1 <- data.frame("genus" = genus, "species" = splitstring)
+  dataframe1
 }
 
 speciesnames("http://ftp.funet.fi/pub/sci/bio/life/insecta/lepidoptera/ditrysia/papilionoidea/papilionidae/papilioninae/lamproptera/")
