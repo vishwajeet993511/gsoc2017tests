d193d18c6a0f0adbeb2073bb545eaad5182c8373
vishwajeet shukla <vishwajeet993511@gmail.com>
2017-03-22 12:29:37 +0530

    output file parameter and family name modification

--- hardpart1.R
+++ hardpart1.R
@@ -1,6 +1,6 @@
   library(stringr)
   data <- read.table("taxo01.txt",sep = "\t")
-  #distribution <- grep("^Distribution", data$V1)
+  distribution <- grep("^Distribution", data$V1)
   namesindex <- grep("^[0-9]",data$V1)
   
   #DISTRIBUTION <- data[distribution,]
@@ -13,15 +13,33 @@
   
    
   #x <- str_locate_all(pattern ='[0-9]. ',stringnew)
-  
+  # determining distribution
   y <- str_locate_all(pattern ='Distribution.*?[0-9.]',stringnew)
   y <- data.frame(y)
   distributionlist <- character(length(y$start))
   for(i in 1:length(y$start))
   distributionlist[i] <- substr(stringnew,y$start[i],y$end[i]-1)
+  
+  # determining familyname
+  familyname <- character(length(data$V1))
+  familyname[1] <- as.character( data$V1[1])
+  for(i in 1:length(data$V1))
+  {
+    if(substr( as.character(data$V1[i]), nchar(as.character(data$V1[i])),
+               nchar(as.character(data$V1[i]))) == "." && grepl("[0-9].",data$V1[i+2])==TRUE
+       )
+      familyname[i+1] <- as.character( data$V1[i+1])
+    else familyname[i+1] <- familyname[i]
+  }
+  
+  family1 <- character(length(distribution))
+  
+  for(i in 1: length(distribution))
+    family1[i] <- familyname[distribution[i]]
    
+  # cleaning 
   distributionlist <- gsub("^Distribution: ","",distributionlist)
-  finaldata <- data.frame("NAME"= NAMES , "DISTRIBUTION"= distributionlist)
+  finaldata <- data.frame("FAMILY" = family1 , "NAME"= NAMES , "DISTRIBUTION"= distributionlist)
   
 
   write.csv(finaldata,"hardpart1formatted_data.csv")
--- hardpart2.R
+++ hardpart2.R
@@ -1,7 +1,7 @@
-tocsv <- function(textfile){
+tocsv <- function(inputfile,outputfile){
   library(stringr)
-  data <- read.table(textfile,sep = "\t")
-  #distribution <- grep("^Distribution", data$V1)
+  data <- read.table(inputfile,sep = "\t")
+  distribution <- grep("^Distribution", data$V1)
   namesindex <- grep("^[0-9]",data$V1)
   
   #DISTRIBUTION <- data[distribution,]
@@ -14,18 +14,37 @@ tocsv <- function(textfile){
   
   
   #x <- str_locate_all(pattern ='[0-9]. ',stringnew)
-  
+  # determining distribution
   y <- str_locate_all(pattern ='Distribution.*?[0-9.]',stringnew)
   y <- data.frame(y)
   distributionlist <- character(length(y$start))
   for(i in 1:length(y$start))
     distributionlist[i] <- substr(stringnew,y$start[i],y$end[i]-1)
   
+  # determining familyname
+  familyname <- character(length(data$V1))
+  familyname[1] <- as.character( data$V1[1])
+  for(i in 1:length(data$V1))
+  {
+    if(substr( as.character(data$V1[i]), nchar(as.character(data$V1[i])),
+               nchar(as.character(data$V1[i]))) == "." && grepl("[0-9].",data$V1[i+2])==TRUE
+    )
+      familyname[i+1] <- as.character( data$V1[i+1])
+    else familyname[i+1] <- familyname[i]
+  }
+  
+  family1 <- character(length(distribution))
+  
+  for(i in 1: length(distribution))
+    family1[i] <- familyname[distribution[i]]
+  
+  # cleaning 
   distributionlist <- gsub("^Distribution: ","",distributionlist)
-  finaldata2 <- data.frame("NAME"= NAMES , "DISTRIBUTION"= distributionlist)
-  write.csv(finaldata2,"hardpart2.csv")
-  finaldata2
+  finaldata <- data.frame("FAMILY" = family1 , "NAME"= NAMES , "DISTRIBUTION"= distributionlist)
+  
+  
+  write.csv(finaldata,outputfile)
   
 }
 
-tocsv("taxo01.txt")
+tocsv("taxo01.txt","tryfile.csv")
